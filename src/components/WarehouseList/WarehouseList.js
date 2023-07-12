b8fc6ef5ec3535bc31f57f465110af5a21d5f0ae
Sebastien <sebdh@hotmail.com>
2023-07-12 14:06:58 -0600

    Added a little styling to see how the page sits and created Link to send user to warehouse details if clicked along with dynamic rendering of entire page for mobile. This code will also take into account any added or deleted warehouses.

---
+++ src/components/WarehouseList/WarehouseList.js
@@ -0,0 +1,83 @@
+import "./WarehouseList.scss";
+import { Link } from "react-router-dom";
+
+const warehouses = [
+  {
+    id: 0,
+    warehouse: "Manhattan",
+    address: "506 Broadway, New York, USA",
+    contactName: "Parmin Aujla",
+    phone: "+1 (629) 555-0129",
+    email: "paujla@instock.com",
+  },
+  {
+    id: 1,
+    warehouse: "Manhattan",
+    address: "506 Broadway, New York, USA",
+    contactName: "Parmin Aujla",
+    phone: "+1 (629) 555-0129",
+    email: "paujla@instock.com",
+  },
+  {
+    id: 2,
+    warehouse: "Manhattan",
+    address: "506 Broadway, New York, USA",
+    contactName: "Parmin Aujla",
+    phone: "+1 (629) 555-0129",
+    email: "paujla@instock.com",
+  },
+  {
+    id: 3,
+    warehouse: "Manhattan",
+    address: "506 Broadway, New York, USA",
+    contactName: "Parmin Aujla",
+    phone: "+1 (629) 555-0129",
+    email: "paujla@instock.com",
+  },
+];
+
+const WarehouseList = () => {
+  return (
+    <main className="main">
+      {/* Will need to add search bar and + Add New Warehouse button 
+          This will be components if I am not mistaken*/}
+      {warehouses.map((warehouse) => {
+        return (
+          <section className="main__section">
+            <div className="main__card">
+              <div className="main__card-info">
+                <div className="main__div">
+                  <p className="main__div-title">WAREHOUSE</p>
+                  <Link key={warehouse.id} to={`/warehouses/${warehouse.id}`}>
+                    <p className="main__div-link">{`${warehouse.warehouse} >`}</p>
+                  </Link>
+                </div>
+                <div className="main__div">
+                  <p className="main__div-title">CONTACT NAME</p>
+                  <p className="main__div-info">{warehouse.contactName}</p>
+                </div>
+              </div>
+              <div className="main__card-info">
+                <div className="main__div">
+                  <p className="main__div-title">ADDRESS</p>
+                  <p className="main__div-info">{warehouse.address}</p>
+                </div>
+                <div className="main__div">
+                  <p className="main__div-title">CONTACT INFORMATION</p>
+                  <p className="main__div-info">{warehouse.phone}</p>
+                  <p className="main__div-info">{warehouse.email}</p>
+                </div>
+              </div>
+              <div className="main__buttons">
+                  <button>trashcan</button>
+                  <button>edit</button>
+              </div>
+            </div>
+          </section>
+        );
+      })}
+    </main>
+  );
+};
+
+export default WarehouseList;
