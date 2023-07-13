a15c394ab4a426ec988d3b6d46f1c668120cad73
frankod123 <frank_od123@yahoo.com>
2023-07-13 20:58:53 +0100

    add main functionality and styling

--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -1,10 +1,74 @@
+import { Link } from "react-router-dom";
+import editIcon from "../../assets/images/Icons/edit-24px.svg";
+import deleteIcon from "../../assets/images/Icons/delete_outline-24px.svg";
+import searchIcon from "../../assets/images/Icons/search-24px.svg";
+import chevron from "../../assets/images/Icons/chevron_right-24px.svg";
 
-const InventoryList = () => {
+const InventoryList = (props) => {
   return (
-    <div>
-        All inventories
-    </div>
-  )
-}
+    <main className="main">
+      <section className="search__container">
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="search__bar"
+          ></input>
+          <img src={searchIcon} className="search__bar-image" alt="search bar icon"/>
+        </div>
+      </section>
+      <container className="addition__container">
+        <Link>
+          <div className="addition">
+            <div className="addition__button">+ Add New Item</div>
+          </div>
+        </Link>
+      </container>
+      {props.inventories.map((inventory) => {
+        return (
+          <section className="main__section">
+            <div className="main__card">
+              <div className="main__card-info">
+                <div className="main__div">
+                  <p className="main__div-title">INVENTORY ITEM</p>
+                  <Link key={inventory.id} to={`/inventory/${inventory.id}`}>
+                    <span className="main__div-link">{`${inventory.item_name}`}</span>
+                    <img className="main__div-link-chevron" src={chevron} alt="right facing chevron"></img>
+                  </Link>
+                </div>
+                <div className="main__div">
+                  <p className="main__div-title">STATUS</p>
+                  <p className="main__div-info">{inventory.status}</p>
+                </div>
+              </div>
+              <div className="main__card-info">
+                <div className="main__div">
+                  <p className="main__div-title">CATEGORY</p>
+                  <p className="main__div-info">{inventory.category}</p>
+                </div>
+                <div className="main__div">
+                  <p className="main__div-title">QTY</p>
+                  <p className="main__div-info">{inventory.quantity}</p>
+                </div>
+              </div>
+              {/* <div className="main__div">
+                  <p className="main__div-title">WAREHOUSE</p>
+                  <p className="main__div-info">{warehouse.warehouse_name}</p>
+                </div> */}
+              <div className="main__buttons">
+                <Link>
+                  <img src={deleteIcon} alt="delete icon"/>
+                </Link>
+                <Link>
+                  <img src={editIcon}  alt="edit icon"/>
+                </Link>
+              </div>
+            </div>
+          </section>
+        );
+      })}
+    </main>
+  );
+};
 
 export default InventoryList
--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,9 +1,20 @@
-import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import InventoryList from "../../components/InventoryList/InventoryList";
 
 function Inventory() {
+
+  const [inventories, setInventory] = useState([]);
+  
+  useEffect(() => {
+    axios.get(`http://localhost:8080/inventories`).then((inventoryList) => {
+      setInventory(inventoryList.data);
+    });
+  }, []);
+  
   return (
     <div>
-      <div>Inventory</div>
+      <InventoryList inventories={inventories} />
     </div>
   );
 }
