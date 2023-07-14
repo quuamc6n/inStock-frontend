168785fdaca351ee43a872e6956a3c45f7038879
Zach Asher <zach.asher@hotmail.co.uk>
2023-07-14 20:45:49 +0300

    Resolved duplicate icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Warehouses from "./pages/Warehouses/Warehouses";
-import SelectedWarehouse from './components/SelectedWarehouse/SelectedWarehouse';
+import SelectedWarehouse from "./components/SelectedWarehouse/SelectedWarehouse";
 import Inventory from "./pages/Inventory/Inventory";
 import HomePage from "./pages/HomePage/HomePage";
 import Footer from "./components/Footer/Footer";
@@ -12,17 +12,34 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="warehouses" element={<Warehouses />}></Route>
-          <Route path="warehouses/add" element={<Warehouses />}></Route>
-          <Route path="warehouses/:warehouseId" element={<SelectedWarehouse />}></Route>
-          <Route path="warehouses/:warehouseId/delete" element={<Warehouses />}></Route>
-          <Route path="warehouses/:warehouseId/edit" element={<Warehouses />}></Route>
-          <Route path="warehouses/:warehouseId/:inventoryId" element={<Warehouses />}></Route>
-          <Route path="inventory" element={<Inventory />}></Route>
-          <Route path="inventory/:inventoryId" element={<Inventory />}></Route>
-        </Routes>
+        <main className="main-section">
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="warehouses" element={<Warehouses />}></Route>
+            <Route path="warehouses/add" element={<Warehouses />}></Route>
+            <Route
+              path="warehouses/:warehouseId"
+              element={<SelectedWarehouse />}
+            ></Route>
+            <Route
+              path="warehouses/:warehouseId/delete"
+              element={<Warehouses />}
+            ></Route>
+            <Route
+              path="warehouses/:warehouseId/edit"
+              element={<Warehouses />}
+            ></Route>
+            <Route
+              path="warehouses/:warehouseId/:inventoryId"
+              element={<Warehouses />}
+            ></Route>
+            <Route path="inventory" element={<Inventory />}></Route>
+            <Route
+              path="inventory/:inventoryId"
+              element={<Inventory />}
+            ></Route>
+          </Routes>
+        </main>
       </BrowserRouter>
       <Footer />
     </div>
