034973e92a153dea9082ab0052f32967853a520f
frankod123 <frank_od123@yahoo.com>
2023-07-15 18:52:18 +0100

    reformat padding and class names

--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -2,92 +2,88 @@ import "./InventoryList.scss";
 import { Link } from "react-router-dom";
 import editIcon from "../../assets/images/Icons/edit-24px.svg";
 import deleteIcon from "../../assets/images/Icons/delete_outline-24px.svg";
-import searchIcon from "../../assets/images/Icons/search-24px.svg";
 import chevron from "../../assets/images/Icons/chevron_right-24px.svg";
 
 const InventoryList = (props) => {
   return (
     <main className="inventory">
-      <section className="inventory__search">
+      <section className="inventory__container">
         <h1 className="inventory__title">Inventory</h1>
-        <section className="search__container">
-          <div className="search">
+        <section className="inventory__search-container">
+          <div className="inventory__search">
             <input
               type="text"
               placeholder="Search..."
-              className="search__bar"
+              className="inventory__search-bar"
             ></input>
-            <img
-              src={searchIcon}
-              className="search__bar-image"
-              alt="search bar icon"
-            />
           </div>
         </section>
-        <container className="addition__container">
+        <div className="inventory__button-container">
           <Link>
-            <div className="addition">
-              <div className="addition__button">+ Add New Item</div>
+            <div className="inventory__button-sub-container">
+              <div className="inventory__button">+ Add New Item</div>
             </div>
           </Link>
-        </container>
+        </div>
       </section>
       {props.inventories.map((inventory) => {
         return (
-          <section className="inventory__section">
-            <div className="inventory__card">
-              <div className="inventory__card-info">
-                <div className="inventory__div">
-                  <p className="inventory__div-title">INVENTORY ITEM</p>
-                  <Link key={inventory.id} to={`/inventory/${inventory.id}`}>
-                    <span className="inventory__div-link">{`${inventory.item_name}`}</span>
+          <article className="inventory__card">
+            <div className="inventory__card-info">
+              <div className="inventory__div">
+                <p className="inventory__div-title">INVENTORY ITEM</p>
+                <Link key={inventory.id} to={`/inventory/${inventory.id}`}>
+                  <div className="inventory__div-container">
+                    <p className="inventory__div-link">{`${inventory.item_name}`}</p>
                     <img
                       className="inventory__div-link-chevron"
                       src={chevron}
                       alt="right facing chevron"
                     ></img>
-                  </Link>
-                </div>
-                <div className="inventory__div">
-                  <p className="inventory__div-title">STATUS</p>
-                  <div className="inventory__div-info--container">
-                  <p className="inventory__div-info">{inventory.status}</p>
                   </div>
-                </div>
+                </Link>
               </div>
-              <div className="inventory__card-info">
-                <div className="inventory__div">
-                  <p className="inventory__div-title">CATEGORY</p>
-                  <p className="inventory__div-info">{inventory.category}</p>
-                </div>
-                <div className="inventory__div">
-                  <p className="inventory__div-title">QTY</p>
-                  <p className="inventory__div-info">{inventory.quantity}</p>
-                </div>
-              </div>
-              <div className="inventory__card-info">
-                <div className="inventory__div inventory__div--ghost">
-                  <p className="inventory__div-title"></p>
-                  <p className="inventory__div-info"></p>
-                </div>
-                <div className="inventory__div">
-                  <p className="inventory__div-title">WAREHOUSE</p>
-                  <p className="inventory__div-info">
-                    {inventory.warehouse_name}
+              <div className="inventory__div">
+                <p className="inventory__div-title">STATUS</p>
+                <div className="inventory__div-info--container">
+                  <p className="inventory__div-info inventory__div-info--status">
+                    {inventory.status}
                   </p>
                 </div>
               </div>
-
-              <div className="inventory__buttons">
-                <Link>
-                  <img src={deleteIcon} alt="delete icon" />
-                </Link>
-                <Link>
-                  <img src={editIcon} alt="edit icon" />
-                </Link>
+            </div>
+            <div className="inventory__card-info">
+              <div className="inventory__div">
+                <p className="inventory__div-title">CATEGORY</p>
+                <p className="inventory__div-info">{inventory.category}</p>
+              </div>
+              <div className="inventory__div">
+                <p className="inventory__div-title">QTY</p>
+                <p className="inventory__div-info">{inventory.quantity}</p>
               </div>
             </div>
-          </section>
+            <div className="inventory__card-info">
+              <div className="inventory__div inventory__div--ghost">
+                <p className="inventory__div-title"></p>
+                <p className="inventory__div-info"></p>
+              </div>
+              <div className="inventory__div">
+                <p className="inventory__div-title">WAREHOUSE</p>
+                <p className="inventory__div-info">
+                  {inventory.warehouse_name}
+                </p>
+              </div>
+            </div>
+
+            <div className="inventory__buttons">
+              <Link>
+                <img src={deleteIcon} alt="delete icon" />
+              </Link>
+              <Link>
+                <img src={editIcon} alt="edit icon" />
+              </Link>
+            </div>
+          </article>
         );
       })}
     </main>
