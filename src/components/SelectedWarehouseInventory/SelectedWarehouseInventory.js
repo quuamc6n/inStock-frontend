7477ab23f18026bb0ac7c2f946852332c50b660f
frankod123 <frank_od123@yahoo.com>
2023-07-15 19:38:08 +0100

    set up structure and make axios call

--- src/components/SelectedWarehouseInventory/SelectedWarehouseInventory.js
+++ src/components/SelectedWarehouseInventory/SelectedWarehouseInventory.js
@@ -1,9 +1,37 @@
+import "./SelectedWarehouseInventory.scss"
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-const SelectedWarehouseInventory = () => {
+const SelectedWarehouseInventory = (props) => {
+  const { warehouseId } = useParams();
+  const [warehouseInventory, setWarehouseInventory] = useState(null);
+ 
+  useEffect(() => {
+    const fetchWarehouseInventory = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/warehouses/${warehouseId}/inventories`
+        );
+        setWarehouseInventory(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchWarehouseInventory();
+  }, [warehouseId]);
+
+  if (!warehouseInventory) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-        Render specific warehouse ID inventory
-    </div>
+    <section>
+      
+
+      
+    </section>
   )
 }
 
