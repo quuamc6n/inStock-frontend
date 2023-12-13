f60c5a0a8ec3deac22d40c37a1a34c37ad205991
quuamc6n <63206589+quuamc6n@users.noreply.github.com>
2023-12-12 17:38:41 -0700

    fixed fetch for warehouseDetails

--- src/components/WarehouseDetails/WarehouseDetails.js
+++ src/components/WarehouseDetails/WarehouseDetails.js
@@ -14,7 +14,7 @@ function WarehouseDetails() {
     const fetchWarehouse = async () => {
       try {
         const response = await axios.get(
-          `https://184f-108-173-235-62.ngrok-free.app/inventories`,
+          `https://184f-108-173-235-62.ngrok-free.app/warehouses/${warehouseId}`,
           {
             headers: {
               "ngrok-skip-browser-warning": "true",
