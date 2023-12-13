500ea9f317734f0933cbc130b2e8f7c0a40824ab
quuamc6n <63206589+quuamc6n@users.noreply.github.com>
2023-12-12 18:37:51 -0700

    Added force reload to fix on delete success

--- src/components/DeleteWarehouse/DeleteWarehouse.js
+++ src/components/DeleteWarehouse/DeleteWarehouse.js
@@ -2,11 +2,8 @@ import "./deletewarehouse.scss";
 import React from "react";
 import CloseIcon from "../../assets/images/Icons/close-24px.svg";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 const DeleteWarehouse = ({ isOpen, onClose, warehouseName, warehouseId }) => {
-  const navigate = useNavigate();
-
   const handleDelete = () => {
     axios
       .delete(
@@ -17,7 +14,7 @@ const DeleteWarehouse = ({ isOpen, onClose, warehouseName, warehouseId }) => {
       )
       .then(() => {
         onClose();
-        navigate('/warehouses')
+        window.location.reload();
       })
       .catch((error) => {
         console.error("Error deleting warehouse:", error);
